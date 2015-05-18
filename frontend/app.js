92e9a0bae215bbf10fff0f227a9326041dc5a469
Samuel Castro <samuelcastrosilva@gmail.com>
2015-05-18 13:16:11 -0300

    Created the initial release of AngularJS project.

---
+++ frontend/app.js
@@ -0,0 +1,23 @@
+angular.module('nGile', ['ui.bootstrap','ui.utils','ui.router','ngAnimate', 'core']);
+
+angular.module('nGile').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/dashboard');
+
+});
+
+angular.module('nGile').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
---
+++ frontend/modules/core/core.js
@@ -0,0 +1,10 @@
+/**
+ * Core Module
+ * @description The Core Module defines all main and general configuration and services of the framework
+ * @author Samuel Castro
+ * @since 5/17/15
+ */
+angular.module('core', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('core').config(function($stateProvider) {});
+
